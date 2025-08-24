84be3aa6b008ab347b25bf2f3d5237ef54e1262c
chavdarajveersinh <152010509+chavdarajveersinh@users.noreply.github.com>
2025-08-24 11:07:20 +0530

    Update apps.py

--- Chasmaapp/apps.py
+++ Chasmaapp/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
 class ChasmaappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Chasmaapp'
+
+    def ready(self):
+        from .models import insert_default_products
+        insert_default_products()
